4a24a0176edaebca4df97c3a64965ee467c25814
Andriy Savych <andriysavych@gmail.com>
2022-11-22 05:28:42 +0100

    improved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import { debounce } from 'lodash';
 import { fetchCountries } from './js/fetchCountries';
 import infoCard from './templates/info_card.hbs';
 import listCard from './templates/list_card.hbs';
-import { debounce } from 'lodash';
 
 
 const searchBoxEl = document.querySelector('#search-box');
@@ -14,10 +14,9 @@ const DEBOUNCE_DELAY = 300;
 
 const onInputChange = event => {
   const searchQuery = event.target.value.trim().toLowerCase();
-  if (searchQuery) {
+  
     fetchCountries(searchQuery)
       .then(data => {
-        let markup;
         if (data.length >= 10) {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
@@ -27,22 +26,17 @@ const onInputChange = event => {
 
           return;
       } else if (data.length > 1) {
-          markup = data.map(el => {
-            return `<li style="list-style: none;"><img src='${el.flags.svg}' width='40' style="margin-right: 20px"/>${el.name.official}</li>`;
-          });
           infoEl.innerHTML = '';
-          listEl.innerHTML = markup.join('');
+          listEl.innerHTML = listCard(data);
         } else {
-          infoEl.innerHTML = '';
-          listEl.innerHTML = infoCard(data[0]);
+          infoEl.innerHTML = infoCard(data[0]);
+          listEl.innerHTML = '';
         }
       })
       .catch(err => {
         Notiflix.Notify.failure('Oops, there is no country with that name');
         searchBoxEl.value = '';
       });
-  }
-  
 }
 
 searchBoxEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY))
